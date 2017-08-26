0cb26bac2e4ee7a26e922474546331aa12080fea
Brad_Hester <hesterbj@iinet.net.au>
2017-08-26 18:25:45 +1000

    Added Thingspeak API's

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ restService.post('/echo', function(req, res) {
     client.getLastEntryInFieldFeed(298464, 1,'',t); //same as API-Method "Retrieving the Last Entry in a Field Feed"
 
 	return res.json({
-        speech: 'The temperature is ' + t + 'degrees',
-        displayText: 'The temperature is ' + t + 'degrees',
+        speech: 'The temperature is ' + t + ' degrees',
+        displayText: 'The temperature is ' + t + ' degrees',
         source: 'Brad Auto Respond'
     });
     //return res.json({
@@ -34,62 +34,6 @@ restService.post('/echo', function(req, res) {
     //    source: 'webhook-echo-sample'
     //});
 });
-/*
-restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "slack": slack_message
-        }
-    });
-});
-*/
-
 
 
 restService.listen((process.env.PORT || 8000), function() {
