b04ad5e8f081c0ec0148e43a39da1e3fda355f12
Brad_Hester <hesterbj@iinet.net.au>
2017-08-26 20:37:58 +1000

    Added Thingspeak API's

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ restService.post('/temperature', function(req, res) {
     // });
 
 	return res.json({
-        speech: 'The temperature is ' + t + ' degrees',
-        displayText: 'The temperature is ' + t + ' degrees',
+        speech: 'The temperature is ',// + t + ' degrees',
+        displayText: 'The temperature is ',// + t + ' degrees',
         source: 'Brad Auto Respond'
     });
     //return res.json({
