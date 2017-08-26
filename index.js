29d52d30bacc883ad1d5aec64f7cd2b612248213
Brad_Hester <hesterbj@iinet.net.au>
2017-08-26 23:17:36 +1000

    Added Thingspeak API's

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
+//https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (res) => {
+//        var body = ''; // Will contain the final response
+//        // Received data is a buffer.
+//    // Adding it to our body
+//     res.on('data', (d) => {
+//        //process.stdout.write(d);
+//        var parsed = JSON.parse(d)
+//        console.log(parsed);
+//        var t = parsed.field1.substring(0, parsed.field1.length-3);
+//        console.log('The temperature is ' + t + ' degrees');
+//        });
+//
+//     })
+
 //var ThingSpeakClient = require('thingspeakclient');
 //	var client = new ThingSpeakClient();
 
@@ -47,14 +61,27 @@ restService.post('/echo', function(req, res) {
 
 restService.post('/temperature', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-	var ThingSpeakClient = require('thingspeakclient');
-	var client = new ThingSpeakClient();
+//	var ThingSpeakClient = require('thingspeakclient');
+//	var client = new ThingSpeakClient();
 
-        client.getLastEntryInFieldFeed(298464, 1, function(err2, response) {
-        if (err2 == null) {
-            var t = response.field1.substring(0, response.field1.length-3);
-            }
+//       client.getLastEntryInFieldFeed(298464, 1, function(err2, response) {
+//        if (err2 == null) {
+//            var t = response.field1.substring(0, response.field1.length-3);
+//            }
+//        });
+
+https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (res) => {
+    // Received data is a buffer.
+    // Adding it to our body
+     res.on('data', (d) => {
+        //process.stdout.write(d);
+        var parsed = JSON.parse(d)
+        console.log(parsed);
+        var t = parsed.field1.substring(0, parsed.field1.length-3);
+//        console.log('The temperature is ' + t + ' degrees');
         });
+
+     })
 
 //return res.json(response.JSON);
 
