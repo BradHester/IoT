ab4b8fcf52194775b6117c2cc50a739e5f033d14
Brad_Hester <hesterbj@iinet.net.au>
2017-08-26 20:31:11 +1000

    Added Thingspeak API's

--- index.js
+++ index.js
@@ -37,13 +37,18 @@ restService.post('/echo', function(req, res) {
 
 restService.post('/temperature', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-	//var ThingSpeakClient = require('thingspeakclient');
-	//var client = new ThingSpeakClient();
+	var ThingSpeakClient = require('thingspeakclient');
+	var client = new ThingSpeakClient();
 
+    client.getLastEntryInFieldFeed(298464, 1, function(err2, response) {
+    if (err2 == null) {
+        var t =  response.field1;
+    }
+});
 
-    https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (response) => {
-     var t = '';
-     });
+    //https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (response) => {
+    // var t = '';
+    // });
 
 	return res.json({
         speech: 'The temperature is ',// + parsed + ' degrees',
