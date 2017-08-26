4e51458f9db50f846f0ed09c5edb00f3b8e03106
Brad_Hester <hesterbj@iinet.net.au>
2017-08-26 22:07:39 +1000

    Added Thingspeak API's

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
+//var ThingSpeakClient = require('thingspeakclient');
+//	var client = new ThingSpeakClient();
+
+//    client.getLastEntryInFieldFeed(298464, 1, function(err2, response) {
+//    if (err2 == null) {
+//        var t = response.field1.substring(0, response.field1.length-3);
+//        console.log('read successfully. value is: ' + response.field1);
+//        console.log('Converted to: ' + t);
+//      });
+
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
 	var ThingSpeakClient = require('thingspeakclient');
@@ -40,11 +50,11 @@ restService.post('/temperature', function(req, res) {
 	var ThingSpeakClient = require('thingspeakclient');
 	var client = new ThingSpeakClient();
 
-    client.getLastEntryInFieldFeed(298464, 3, function(err2, response) {
-    if (err2 == null) {
-        console.log('read successfully. value is: ' + response.field3);
-    }
-});
+        client.getLastEntryInFieldFeed(298464, 1, function(err2, response) {
+        if (err2 == null) {
+            var t = response.field1.substring(0, response.field1.length-3);
+            }
+        });
 
 //return res.json(response.JSON);
 
