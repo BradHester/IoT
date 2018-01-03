f442642124022207df43b1cc514a7f85aed04886
Brad_Hester <hesterbj@iinet.net.au>
2018-01-03 16:43:08 +1100

    Moving to reference Configs

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict';
 let https = require('https');
-
+var gardenconfig = require("config.json");
 exports.handler = function(event, context, callback) {
 
 var temperaturereturn = function() {
 return new Promise((resolve, reject) => {
     console.log('Starting Temperature...');
+        console.log(gardenconfig.Temperature.APIURL);
         https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (response) => {
         response.on('data', (d) => {
             var parsed = JSON.parse(d);
             //console.log(parsed);
             var t = parsed.field1.substring(0, parsed.field1.length-3);
-            let response = "the temperature is " + t + " degrees";
+            let response = gardenconfig.Temperature.Prefix + t + gardenconfig.Temperature.Suffix;
             console.log(response);
            resolve(response);
         });
