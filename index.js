1b0cab4d5f71d2d4eecc35df0f280cd2aa744b6d
Brad_Hester <hesterbj@iinet.net.au>
2018-01-03 16:14:35 +1100

    Rewritten for Lambda

--- index.js
+++ index.js
@@ -1,99 +1,72 @@
 'use strict';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('http');
-const https = require('https');
-const restService = express();
-
-restService.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-restService.use(bodyParser.json());
-
-//const req = https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (response) => {
-//    // Received data is a buffer.
-//    // Adding it to our body
-//    response.on('data', (d) => {
-//        //process.stdout.write(d);
-//        var parsed = JSON.parse(d)
-//        console.log (parsed);
-//        var t = parsed.field1.substring(0, parsed.field1.length-3);
-//        var z = "{'speech':" + "'The temperature is " + t + " degrees'" + ",'displayText':'The temperature is "  + t + " degrees'" + ", 'source':'Brad Auto Respond'}";
-//        //console.log(z);
-//
-//        var obj = JSON.parse(z)
-//
-//        });
-//
-//     })
-//req.on('error',(e) => {
-//        t = e;
-//    });
-//req.end();
+let https = require('https');
 
 
-//var ThingSpeakClient = require('thingspeakclient');
-//	var client = new ThingSpeakClient();
 
-//    client.getLastEntryInFieldFeed(298464, 1, function(err2, response) {
-//    if (err2 == null) {
-//        var t = response.field1.substring(0, response.field1.length-3);
-//        console.log('read successfully. value is: ' + response.field1);
-//        console.log('Converted to: ' + t);
-//      });
+exports.handler = function(event, context, callback) {
 
-restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-	//var ThingSpeakClient = require('thingspeakclient');
-	//var client = new ThingSpeakClient();
-
-	//client.attachChannel(298464, { readKey:'A1FE5T3THYNCRH05'});
-    var t = '18';
-    //client.getLastEntryInFieldFeed(298464, 1, t);
-
-    //client.getLastEntryInFieldFeed(298464, 1); //same as API-Method "Retrieving the Last Entry in a Field Feed"
-
-	return res.json({
-        speech: 'The temperature is ' + t + ' degrees',
-        displayText: 'The temperature is ' + t + ' degrees',
-        source: 'Brad Auto Respond'
-    });
-    //return res.json({
-    //   speech: speech,
-    //    displayText: speech,
-    //    source: 'webhook-echo-sample'
-    //});
-});
-
-restService.post('/temperature', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var t = 'Error';
-
-    const req1 = https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (response) => {
-    // Received data is a buffer.
-    // Adding it to our body
-     //console.log(response);
-     response.on('data', (d) => {
-        //process.stdout.write(d);
-        var parsed = JSON.parse(d)
-        var t = parsed.field1.substring(0, parsed.field1.length-3);
-        console.log('The temperature is ' + t + ' degrees');
-        return res.json({
-            speech: 'The temperature is ' + t + ' degrees',
-            displayText: 'The temperature is '  + t + ' degrees',
-            source: 'Brad Auto Respond'
-            });
+        
+var temperaturereturn = function() {
+return new Promise((resolve, reject) => {
+    console.log('Starting Temperature...');
+        https.get("https://api.thingspeak.com/channels/298464/fields/1/last.json", (response) => {
+        response.on('data', (d) => {
+            var parsed = JSON.parse(d);
+            //console.log(parsed);
+            var t = parsed.field1.substring(0, parsed.field1.length-3);
+            let response = "the temperature is " + t + " degrees";
+            console.log(response);
+           resolve(response);
         });
-
-
-
-
+        });    
+}).then(function(data){
+    return data
 });
+};
 
-});
+var humidityreturn = function() {
+return new Promise((resolve, reject) => {
+    console.log('Starting Humidity...');
+        https.get("https://api.thingspeak.com/channels/298464/fields/2/last.json", (response) => {
+        response.on('data', (d) => {
+            var parsed = JSON.parse(d);
+            //console.log(parsed);
+            var t = parsed.field2.substring(0, parsed.field2.length-3);
+            let response = "the humidity is " + t + " percent";
+            console.log(response);
+            resolve(response);
+        });
+        });    
+    }).then(function(data){
+        return data
+    });
+    };
 
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
-});
+    console.log("Starting Query");
+    console.log( "Intent:" + event.result.metadata.intentName + " received");
+    
+    if(event.result.metadata.intentName == "Temperature") {
+        Promise.all([temperaturereturn()]).then(function (data){
+            callback(null, {"speech": data[0]});
+        });
+        
+    } 
+            
+
+    else if (event.result.metadata.intentName == "Humidity") {
+       Promise.all([humidityreturn()]).then(function (data){
+            callback(null, {"speech": data[0]});
+        });
+    }
+    else if (event.result.metadata.intentName == "Temperature and Humidity") {
+        Promise.all([temperaturereturn(), humidityreturn()]).then(function (data){
+            callback(null, {"speech": data[0] + " and " + data[1]});
+        });    
+    }
+    
+    
+    else {
+        console.log('Unknown Request received');
+        callback(null, {"speech": "Unknown Request"});
+    }
+};
